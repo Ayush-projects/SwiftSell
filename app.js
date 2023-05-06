fb1f19a3e42bdf3d86e5d5b0cba00271f1b05ad0
Ayush-projects <kayush479@gmail.com>
2023-05-06 09:09:54 +0530

    Add express and other dependencies

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const PORT = process.env.PORT || 3000
+
+
+app.get("/", (req,res)=>{
+    res.status()
+})
+
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log("Server is listening on:", PORT)
+})
